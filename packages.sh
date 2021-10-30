4e7d2d7e1c24cd2c3139e764dbddcd9e8cb60725
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2021-10-30 14:00:58 +0300

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#######################################
+# Bash script to install apps on a new system (Arch Linux)
+# Written by @omertahaoztop from https://omertahaoztop.com
+#######################################
+
+## Update packages and Upgrade system
+sudo pacman -Syyuu
